54a71d4e8f4999cc65f0ff95efeac6b695621e21
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-07 10:51:10 +0200

    Add files via upload

---
+++ Application-Basics/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.aab6hw2
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.aab6hw2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        var count = 0
+        binding.buttonSet.setOnClickListener{ view ->
+            count++
+            binding.textView.text = "$count. ${binding.editTextInput.text}"
+            binding.editTextInput.setText("")
+        }
+
+        binding.buttonUndo.setOnClickListener{ view ->
+            count--
+            val textView = binding.textView.text
+            binding.editTextInput.setText(textView.subSequence(3, textView.length))
+            binding.textView.text = ""
+        }
+
+        binding.buttonReset.setOnClickListener{ view ->
+            count = 0
+            binding.textView.text = ""
+            binding.editTextInput.setText("")
+        }
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "test successful", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
